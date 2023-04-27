f48f629b5d78ec59e88ca7d48ac113ec9f8ab230
vencax <vencax77@gmail.com>
2023-04-27 15:22:43 +0200

    config in DB

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
 import { defineEndpoint } from '@directus/extensions-sdk';
 import { verify } from 'hcaptcha';
-import getConfig from './config';
+
+interface HCaptchaConfig { 
+    secret_key: string; 
+    remote_ip: string | null;
+	site_key: string | null;
+	token: string;
+} 
 
 export default defineEndpoint({
 	id: 'hcaptcha-validate',
-	handler: (router) => {
-		router.get('/:webid/:token', async (req :any, res: any) => {
+	handler: (router, { database, exceptions }) => {
+		const { ServiceUnavailableException } = exceptions;
+		
+		router.get('/:id([0-9]+)/:token', async (req :any, res: any) => {
 			try {
-				const config = await getConfig(req.params.webid)
+				const config: HCaptchaConfig = await database('hcaptchainfo').where({ 
+					id: req.params.id
+				}).first()
 				const data = await verify(
-					config.HCAPTCHA_SECRETKEY, 
+					config.secret_key, 
 					req.params.token, 
-					config.HCAPTCHA_REMOTEIP, 
-					config.HCAPTCHA_SITEKEY
+					config.remote_ip || undefined, 
+					config.site_key || undefined
 				)
 				if (!data.success) {
 					throw data
 				}
 				res.send(config.token)			
-			} catch (err) {
-				res.status(400).send('invalid token')
+			} catch (err: any) {
+				throw new ServiceUnavailableException(err.message);
 			}
 		});
 	}
